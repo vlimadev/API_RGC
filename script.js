dc32f2e5b3d8696b2ebef9b80fefeabca58fa3e7
Vinicius Lima <vinilimadev@gmail.com>
2022-05-13 10:33:17 -0300

    cometarios js

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
-
+// caminho para o banco de dados
 const uri = 'http://localhost:3000/produtos'
 
-
+// função que faz o GET no banco e apresenta resultado na tela
  const carregarProdutos = async()=>{
 
  
     const uriFazGet = await fetch (uri)
-    const dados = await uriFazGet.json()
+    const dados = await uriFazGet.json() //tranforma os dados obtidos em json
     console.log (dados)
    
+    // transforma os dados em  itens
     dados.forEach(item => {
    
        const containerProdutoItem = document.getElementById ('container-produtos')
-   
        const template = document.getElementById('temp_produtos')
-   
        const ProdutoItem = document.importNode(template.content, true)
-   
        const itensProduto = ProdutoItem.querySelectorAll('span')
    
+       //insere cada item em um campo no html
        itensProduto[0].innerText = item.id
        itensProduto[1].innerText = item.codigo
-       itensProduto[2].innerText = item.descrição
+       itensProduto[2].innerText = item.descrição   
        itensProduto[3].innerText = item.valor
    
        containerProdutoItem.append(ProdutoItem)
@@ -37,10 +36,10 @@ const uri = 'http://localhost:3000/produtos'
    const _id = document.getElementById('id')
 
 
-
+// faz o POST de um novo produto
 const novoProduto =  async() =>{
    
-    
+    // fromatção enviada para o banco de dados
     const produtos = {
         
         id :_id.value,
@@ -50,24 +49,26 @@ const novoProduto =  async() =>{
 
     }
 
+    // chama o método POST 
     const init = {
         method: 'POST',
         headers:{"Content-Type" : 'application/json; charset=utf-8'},
 
-        body: JSON.stringify(produtos)
+        body: JSON.stringify(produtos) //envia o corpo como JSON
     }
     
     const resposta = await fetch (uri , init)
    
-    
-
 }
 
-const btnEnvia = document.getElementById('enviar')
+// configurações do botão de enviar
+const btnEnvia = document.getElementById('enviar') 
 btnEnvia.onclick = novoProduto
 
+// função que edita o produto
 const alteraProduto = async ( ) =>{
 
+    // corpo enviado para o banco de dados
     const produtos = {
         
         id :_id.value,
@@ -76,6 +77,7 @@ const alteraProduto = async ( ) =>{
         valor : Number (_valor.value)
     }
 
+    //chama o método PUT e passa o ID digitado para edição
     const response = await fetch(uri +"/"+_id.value, {
         method: 'PUT', 
         headers: {
@@ -89,21 +91,25 @@ const alteraProduto = async ( ) =>{
     console.log(data);
  };
  
+ // configurações do botão alterar
  const btnAltera = document.getElementById('alterar')
  btnAltera.onclick = alteraProduto
 
+//função que faça o DELETE
  const remove = async () => {
 
+    //Chama o DELETE e passa o ID como chave para exclusão
     await fetch(uri + "/" + _id.value ,{
 
       method: 'DELETE',
      
  })}
 
+ //configurações do botão de apagar
  const btnApagar = document.getElementById('apagar')
  btnApagar.onclick = remove
  
- 
+ //chamando função 
  carregarProdutos(uri)
 
 
